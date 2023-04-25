d04c823d5437e93a00300a640c7c116ff8ec0dbc
Himanshu Gupta <himanshugupta558@gmail.com>
2023-04-25 13:01:50 +0530

    Adding react module to write jsx directly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from './components/Expenses';
-
+import React from 'react';
 function App() {
   const expenses = [
     {
@@ -27,12 +27,18 @@ function App() {
       date: new Date(2022, 5, 12),
     },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses items = {expenses} />
-    </div>
-  );
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, 'Let\'s get started!'),
+    React.createElement(Expenses, { items: expenses})
+  ); 
+
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses items = {expenses} />
+  //   </div>
+  // );
 }
 
 export default App;
