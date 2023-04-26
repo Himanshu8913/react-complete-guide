e1814d3a3b0cfeb84e30b909b7e5c49636a36746
Himanshu Gupta <himanshugupta558@gmail.com>
2023-04-26 11:48:02 +0530

    testing onClick event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -10,6 +10,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
       <div className='expense-item__price'>INR {props.amount}</div>
       </div>
+      <button onClick={() => {console.log('CLICKED')}}>Change Title</button>
     </Card>
   );
 }
