2be13324cd6335fcf6e3768f5fd1c16b4fbf1dfc
Himanshu Gupta <himanshugupta558@gmail.com>
2023-05-03 11:38:56 +0530

    fix code formate

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate.js';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
@@ -7,10 +7,10 @@ const ExpenseItem = (props) => {
 
   return (
     <Card className='expense-item'>
-      <ExpenseDate date = {props.date} />
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
-      <div className='expense-item__price'>INR {props.amount}</div>
+        <div className='expense-item__price'>INR {props.amount}</div>
       </div>
     </Card>
   );
